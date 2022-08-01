47dfdeec5889ccafff4773f2b5133b48d8ffdd79
Guilherme Miserski <guilhermemiserski@hotmail.com>
2022-08-01 11:35:57 -0300

    alterando about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -6,7 +6,9 @@ export function About() {
     <section className="about" id="about">
       <div>
         <h2>O que é a Skål?</h2>
-        <p>O projeto visa desenvolver um sistema para venda e controle de ingressos tokenizados, aproveitando todo o aparato criptográfico que os blockchains disponibilizam, conseguindo solucionar dores como burocracia na compra de ingressos, falsificações e também redução de custos, além de gerar vantagens exclusivas e royalties à medida que as NFTs se tornam cobráveis ​​e com maior valor de mercado.</p>
+        <p>A Skål é um sistema para venda e gestão de ingressos tokenizados na
+          rede Polygon, nós queremos proporcionar maior conectividade entre
+          artistas, fãs, organizadores e marcas criando experiências mais imersivas. Além de gerar vantagens exclusivas e royalties à medida que as NFTs se tornam cobráveis ​​e com maior valor de mercado.</p>
         <Cards />
       </div>
     </section>
