69677cc5693257d0a4a4ebe9b85fe93174a55e71
Guilherme Miserski <guilherme.miserski@softfocus.com.br>
2022-09-19 12:48:30 -0300

    adicionando fetch

--- src/components/Updates/index.tsx
+++ src/components/Updates/index.tsx
@@ -2,10 +2,12 @@ import './style.css';
 import { ToastContainer, toast } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
 
 export function Updates() {
+  const [email, setEmail] = useState("");
 
-  const notify = () => toast.success('Enviado!', {
+  const notifySucess = () => toast.success('Enviado!', {
     position: "bottom-right",
     autoClose: 3000,
     hideProgressBar: false,
@@ -15,14 +17,44 @@ export function Updates() {
     progress: undefined,
   })
 
-  
+  const notifyFailure = () => toast.error('Não Enviado!', {
+    position: "bottom-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+
+  const saveEmail = () => {
+    fetch('http://127.0.0.1:8000/users/', {
+      method: 'POST',
+      body: JSON.stringify({
+        email
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then(res => {
+        if (res.status === 201) {
+          notifySucess();
+        }
+        else {
+          notifyFailure();
+        }
+      });
+  }
+
+
   return (
     <section className="updates">
       <div>
         <h2>Fique ligado nas próximas <span className='text-effect'>atualizações</span>!</h2>
         <div className="send-email">
-          <input type="email" placeholder="Digite seu email..." />
-          <button className="btn btn-primary"onClick={notify}>Enviar</button>
+          <input type="email" placeholder="Digite seu email..." onChange={(e) => setEmail(e.target.value)} />
+          <button className="btn btn-primary" onClick={saveEmail}>Enviar</button>
           <ToastContainer />
         </div>
       </div>
